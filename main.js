7ec0a856ab47fdba05722df252f646776f9ce152
Grace041 <chisum.lau@student.uts.edu.au>
2026-03-27 19:29:17 +1100

    created some files + folder for later coding
    feel free to add more files/folder if you need them

---
+++ main.js
@@ -0,0 +1,3 @@
+import * as THREE from "/build/three.module.js";
+
+import {scene, camera, renderer} from "/setup.js";
---
+++ setup.js
@@ -0,0 +1,5 @@
+import * as THREE from "/build/three.module.js"
+
+export let scene;
+export let camera;
+export let renderer;
