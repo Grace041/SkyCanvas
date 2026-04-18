3b8111eea0351594f6241775611a0f501f56c70c
Gejia <gejialan@gmail.com>
2026-04-18 22:33:30 +1000

    Import the model into the scene

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-import * as THREE from "/build/three.module.js";
-import { OrbitControls } from "/build/controls/OrbitControls.js";
-import { scene, camera, renderer, setScene, setSceneElements, setSceneLighting } from "/setup.js";
-
+import * as THREE from "./build/three.module.js";
+import { OrbitControls } from "./build/controls/OrbitControls.js";
+import { scene, camera, renderer, setScene, setSceneElements, setSceneLighting, loadBackgroundModels } from "./setup.js";
 setScene();
 setSceneElements();
 setSceneLighting();
-
+loadBackgroundModels();
 const controls = new OrbitControls(camera, renderer.domElement);
-
 function updateScene() {
     controls.update();
     renderer.render(scene, camera);
 }
-
 renderer.setAnimationLoop(updateScene);
