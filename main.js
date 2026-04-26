89e32e5a4d9aae67057a7d053c7d2e5c4a858f77
Gejia <gejialan@gmail.com>
2026-04-26 22:46:45 +1000

    Change the background model

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-import * as THREE from "./build/three.module.js";
-import { OrbitControls } from "./build/controls/OrbitControls.js";
-import { scene, camera, renderer, setScene, setSceneElements, setSceneLighting, loadBackgroundModels } from "./setup.js";
+import{ OrbitControls } from "./build/controls/OrbitControls.js";
+import{scene, camera, renderer, setScene, setSceneElements, setSceneLighting, loadBackgroundModels} from "./setup.js";
 setScene();
 setSceneElements();
 setSceneLighting();
 loadBackgroundModels();
 const controls = new OrbitControls(camera, renderer.domElement);
-function updateScene() {
+controls.enableDamping = true;
+camera.position.set(900, 300, 900);
+controls.target.set(-1500, 0, -20);
+controls.update();
+function updateScene(){
     controls.update();
     renderer.render(scene, camera);
 }
